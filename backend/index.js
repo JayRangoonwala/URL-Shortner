af2e35a612b187bc25b63c96797116c77fa2cee6
Jay <jaymrangoon57@gmail.com>
2025-04-03 14:51:52 +0530

    final commit1

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,45 @@
-import express from 'express';
-import { Connect } from './connection.js';
-import { router } from './routes/url.js';
-import {userrouter} from './routes/user.js'
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import { analyticRouter } from './routes/analytics.js';
-import { qrrouter } from './routes/qrcode.js';
+import express from "express";
+import { Connect } from "./connection.js";
+import { router } from "./routes/url.js";
+import { userrouter } from "./routes/user.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { analyticRouter } from "./routes/analytics.js";
+import { qrrouter } from "./routes/qrcode.js";
 
 const app = express();
 const PORT = 8000;
+const allowedOrigins = ["http://localhost:3000", "https://tinyyqr.vercel.app"];
 
 app.use(express.json());
-app.use(cors({origin:"https://tinyyqr.vercel.app", credentials: true }));
-app.use(express.urlencoded({extended:true}));
-app.use(cookieParser())
 
-app.use("/url",router);
-app.use("/user",userrouter);
-app.use("/analysis",analyticRouter);
-app.use("/qrcode",qrrouter);
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 
-Connect(process.env.MONGODB_URI)
-.then(() => console.log("MongoDb Connected !!"));
+app.options("*", cors()); // Handle preflight requests
 
-app.listen(PORT ,(req, res) => {
-    console.log(`Server is Running on PORT : ${PORT}`);
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use("/url", router);
+app.use("/user", userrouter);
+app.use("/analysis", analyticRouter);
+app.use("/qrcode", qrrouter);
+
+Connect(process.env.MONGODB_URI).then(() =>
+  console.log("MongoDb Connected !!")
+);
+
+app.listen(PORT, (req, res) => {
+  console.log(`Server is Running on PORT : ${PORT}`);
 });
